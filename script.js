21ffa3ca98d34f957c87c234962c439c88793823
Cleiber Ziviani <cleiberz@yahoo.com.br>
2021-04-16 11:22:47 -0300

    movimentos criados

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ snake[0] = {
     y: 8 * box
 }
 
+let direction = 'rigth';
+
 function criarBG() {
     context.fillStyle = 'lightgreen';
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -21,5 +23,20 @@ function criarCobrinha() {
     }
 }
 
-criarBG();
-criarCobrinha();
+function inicarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction == 'right') snakeX += box;
+    if (direction == 'left') snakeX -= box;
+    if (direction == 'up') snakeY -= box;
+    if (direction == 'down') snakeY += box;
+
+    snake.pop();
+ }
+
+let jogo = setInterval(iniciarJogo, 100);
+
